c3e5747532104950fe127de60e5f6acfb0e5ee4a
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-04-13 10:55:49 +0300

    Add currentTurn and winner fields to Game entity

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Model\GameStatus;
+use App\Model\GameTurn;
 use App\Repository\GameRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
@@ -19,12 +20,31 @@ class Game
 
     #[ORM\Column]
     private array $board = [];
+    /*
+        0 | 1 | 2
+        ---------
+        3 | 4 | 5
+        ---------
+        6 | 7 | 8
+     */
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(enumType: GameStatus::class)]
     private ?GameStatus $status = null;
+
+    #[ORM\Column(enumType: GameTurn::class)]
+    private ?GameTurn $currentTurn = null;
+
+    #[ORM\Column(nullable: true, enumType: GameTurn::class)]
+    private ?GameTurn $winner = null;
+
+    public function __construct()
+    {
+        $this->currentTurn = GameTurn::X_TURN;
+        $this->createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?Uuid
     {
@@ -63,6 +83,30 @@ class Game
     public function setStatus(GameStatus $status): static
     {
         $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCurrentTurn(): ?GameTurn
+    {
+        return $this->currentTurn;
+    }
+
+    public function setCurrentTurn(GameTurn $currentTurn): static
+    {
+        $this->currentTurn = $currentTurn;
+
+        return $this;
+    }
+
+    public function getWinner(): ?GameTurn
+    {
+        return $this->winner;
+    }
+
+    public function setWinner(?GameTurn $winner): static
+    {
+        $this->winner = $winner;
 
         return $this;
     }
